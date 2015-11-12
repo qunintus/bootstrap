b451ab3b754ac588f179e58a1b74af1a75128c5d
XhmikosR <xhmikosr@gmail.com>
2015-11-12 21:32:19 +0200

    Update grunt-contrib-uglify to the latest version.
    Adapt Gruntfile.js for the comments change.

--- Gruntfile.js
+++ Gruntfile.js
@@ -185,7 +185,7 @@ module.exports = function (grunt) {
           warnings: false
         },
         mangle: true,
-        preserveComments: 'some'
+        preserveComments: /^!|@preserve|@license|@cc_on/i
       },
       core: {
         src: '<%= concat.bootstrap.dest %>',
